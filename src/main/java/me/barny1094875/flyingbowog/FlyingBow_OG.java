c9a5c244fece49d7c9ddf4fb95416a4bd9282d8c
Barny1094875 <christian.niehaus000@gmail.com>
2023-03-24 20:50:03 -0400

    The first commit I ever made

---
+++ src/main/java/me/barny1094875/flyingbowog/FlyingBow_OG.java
@@ -0,0 +1,66 @@
+package me.barny1094875.flyingbowog;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+public final class FlyingBow_OG extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        // create the flying bow item
+        ItemStack flyingBowItem = new ItemStack(Material.BOW);
+        ItemMeta bowMeta = flyingBowItem.getItemMeta();
+        bowMeta.setDisplayName(ChatColor.AQUA + ChatColor.ITALIC.toString() +  "Flying Bow");
+        flyingBowItem.setItemMeta(bowMeta);
+        // adding a useless itemFlag to identify this as a flying bow
+        // this was easier for me than adding an attribute, and no other bow should have
+        // this flag
+        flyingBowItem.addItemFlags(ItemFlag.HIDE_DYE);
+
+
+        // add the recipe into the game
+        ShapedRecipe flyingBowRecipe = new ShapedRecipe(new NamespacedKey(this, "flying_bow"),
+                flyingBowItem);
+        flyingBowRecipe.shape("sd ", "sed", "sd ");
+        flyingBowRecipe.setIngredient('s', Material.STICK);
+        flyingBowRecipe.setIngredient('e', Material.ELYTRA);
+        flyingBowRecipe.setIngredient('d', Material.DIAMOND);
+
+
+        Bukkit.addRecipe(flyingBowRecipe);
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onEntityShootBow(EntityShootBowEvent event){
+        // if the entity that shot the bow is a player
+        if(event.getEntity() instanceof Player){
+            // if the player is holding a flying bow
+            if(((Player) event.getEntity())
+                    .getInventory()
+                    .getItemInMainHand()
+                    .getItemFlags().contains(ItemFlag.HIDE_DYE)){
+                event.getProjectile().addPassenger(event.getEntity());
+            }
+        }
+    }
+}

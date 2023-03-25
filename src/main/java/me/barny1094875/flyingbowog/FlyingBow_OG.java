a937b2a48e2e9cd2cd06339acffa2051569943cd
Barny1094875 <christian.niehaus000@gmail.com>
2023-03-25 18:24:04 -0400

    Fix a 1.8.9 bug

--- src/main/java/me/barny1094875/flyingbowog/FlyingBow_OG.java
+++ src/main/java/me/barny1094875/flyingbowog/FlyingBow_OG.java
@@ -2,13 +2,13 @@ package me.barny1094875.flyingbowog;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,8 +18,10 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
-import java.util.List;
+import java.util.Collections;
+
 
 public final class FlyingBow_OG extends JavaPlugin implements Listener {
 
@@ -30,8 +32,13 @@ public final class FlyingBow_OG extends JavaPlugin implements Listener {
         // create the flying bow item
         ItemStack flyingBowItem = new ItemStack(Material.BOW);
         ItemMeta bowMeta = flyingBowItem.getItemMeta();
-        bowMeta.setDisplayName(ChatColor.AQUA + ChatColor.ITALIC.toString() +  "Flying Bow");
+        bowMeta.displayName(Component.text("")
+                .color(TextColor.color(0, 255, 255))
+                .decorate(TextDecoration.ITALIC)
+                .content("Flying Bow"));
+        bowMeta.lore(Collections.singletonList(Component.text("Shoot this bow to FLY")));
         flyingBowItem.setItemMeta(bowMeta);
+
         // adding a useless itemFlag to identify this as a flying bow
         // this was easier for me than adding an attribute, and no other bow should have
         // this flag
@@ -53,14 +60,35 @@ public final class FlyingBow_OG extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onEntityShootBow(EntityShootBowEvent event){
+
+        ItemStack eventBow =((Player) event.getEntity())
+                            .getInventory()
+                            .getItemInMainHand();
+        Entity eventEntity = event.getEntity();
+        Location eventEntityLocation = eventEntity.getLocation();
+        Entity eventArrow = event.getProjectile();
+
         // if the entity that shot the bow is a player
-        if(event.getEntity() instanceof Player){
+        if(eventEntity instanceof Player){
+
             // if the player is holding a flying bow
-            if(((Player) event.getEntity())
-                    .getInventory()
-                    .getItemInMainHand()
-                    .getItemFlags().contains(ItemFlag.HIDE_DYE)){
-                event.getProjectile().addPassenger(event.getEntity());
+            if(eventBow.getItemFlags()
+                    .contains(ItemFlag.HIDE_DYE)){
+
+                // eject the player from the previous arrow, if they were riding
+                // on one before
+                if (eventEntity.getVehicle() != null) {
+                    eventEntity.getVehicle().removePassenger(eventEntity);
+                }
+
+                // tp the player to the location they were at when the arrow was shot 2 ticks after it
+                // was shot to force the server to track the player's position
+                // then make the player ride the arrow
+                Bukkit.getScheduler().runTaskLater(this, () -> {
+                    eventEntity.teleport(eventEntityLocation);
+                    eventArrow.addPassenger(eventEntity);
+                }, 2L);
+
 
                 // if the bow has infinity on it, remove infinity
 
@@ -72,15 +100,10 @@ public final class FlyingBow_OG extends JavaPlugin implements Listener {
                 // even though mending can't be put on a bow with infinity, grindstones exist
                 // and an infinity bow can be made to last forever, it just takes some time
                 // at an exp farm to repair it
-                if((((Player) event.getEntity())
-                        .getInventory()
-                        .getItemInMainHand()
-                        .getEnchantments().containsKey(Enchantment.ARROW_INFINITE))){
+                if((eventBow.getEnchantments()
+                        .containsKey(Enchantment.ARROW_INFINITE))){
                     // remove infinity from the bow
-                    ((Player) event.getEntity())
-                            .getInventory()
-                            .getItemInMainHand()
-                            .removeEnchantment(Enchantment.ARROW_INFINITE);
+                    eventBow.removeEnchantment(Enchantment.ARROW_INFINITE);
                 }
             }
         }
